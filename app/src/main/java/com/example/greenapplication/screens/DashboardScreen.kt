be645d87b631ff74b0313671ec52b45f6ba608d1
lucapauri <luca.pauri9@gmail.com>
2022-11-15 11:59:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/greenapplication/screens/DashboardScreen.kt
@@ -0,0 +1,21 @@
+package it.polito.did.gameskeleton.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import it.polito.did.gameskeleton.ui.theme.GameSkeletonTheme
+
+@Composable
+fun DashboardScreen(modifier: Modifier = Modifier) {
+    GenericScreen(title = "Dashboard", modifier) {
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewDashboardScreen() {
+    GameSkeletonTheme {
+        DashboardScreen()
+    }
+}
