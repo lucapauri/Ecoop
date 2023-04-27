188ade5fc81e95e0cfd141765b18d7ed2e17191b
silviazappia <silviazappia96@gmail.com>
2023-04-27 16:11:51 +0200

    import_regolamento

--- app/src/main/java/com/example/greenapplication/MainActivity.kt
+++ app/src/main/java/com/example/greenapplication/MainActivity.kt
@@ -6,16 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.greenapplication.pag1.Pag1
 import com.example.greenapplication.pag2registrati.Pag2Registrati
-import com.google.relay.compose.BoxScopeInstance.rowWeight
-import com.google.relay.compose.BoxScopeInstance.columnWeight
-import it.polito.did.gameskeleton.screens.MainScreen
-import it.polito.did.gameskeleton.screens.GenericScreen
 import it.polito.did.gameskeleton.ui.theme.GameSkeletonTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +24,6 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Pag1()
-                    Pag2Registrati()
                 }
             }
         }
@@ -42,10 +36,6 @@ fun PreviewFrame() {
     GameSkeletonTheme {
 
         Pag1()
-        Pag2Registrati(
-            nome = "Inserisci nickname",
-            textContent = "scrivi qualcosa",
-            modifier = Modifier.rowWeight(1.0f).columnWeight(1.0f))
     }
 
 }
