02cdaeb41f7c10979fc615fee976dcbb709d39b3
silviazappia <silviazappia96@gmail.com>
2023-05-18 16:48:58 +0200

    PollScreen

---
+++ app/src/main/java/com/example/greenapplication/screens/PollScreen.kt
@@ -0,0 +1,78 @@
+package com.example.greenapplication.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.greenapplication.pag1.Text
+import it.polito.did.gameskeleton.ui.theme.GameSkeletonTheme
+
+@Composable
+fun PollScreen(){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 470.dp, start = 15.dp, end = 15.dp)
+            .clickable { },
+        elevation = 10.dp
+    ){
+        Column(
+            modifier = Modifier.padding(15.dp), horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            androidx.compose.material.Text(
+                text = "Sondaggio",
+                style = MaterialTheme.typography.subtitle1,
+                color = MaterialTheme.colors.background,
+                modifier = Modifier.padding(bottom = 10.dp)
+            )
+
+            androidx.compose.material.Text(
+                text = "Proposta inserimento infrastruttura blablabla: \nSei favorevole a questa scelta? ",
+                style = MaterialTheme.typography.subtitle2,
+                color = MaterialTheme.colors.onBackground,
+                textAlign = TextAlign.Center
+            )
+
+            Row() {
+                Button(onClick = { /*TODO*/ },
+                    shape = RoundedCornerShape(20.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red),
+                modifier = Modifier.padding(25.dp)) {
+                    androidx.compose.material.Text(text = "", color = Color.White)
+                    Icon(imageVector = Icons.Default.Close, "",
+                        tint = Color.White)
+                }
+                Button(onClick = { /*TODO*/ },
+                    shape = RoundedCornerShape(20.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.background),
+                    modifier = Modifier.padding(25.dp)) {
+                    androidx.compose.material.Text(text = "", color = Color.White)
+                    Icon(imageVector = Icons.Default.Check, "",
+                        tint = Color.White)
+                }
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun PreviewPollScreen(){
+    GameSkeletonTheme {
+        MapScreen(SelectedIcon = 2)
+        GridScreen(team = "Quartiere Rosso")
+        PollScreen()
+    }
+}
