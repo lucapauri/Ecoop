c8ed8546c456027229b361b12fb85bb3eda3d0b8
silviazappia <silviazappia96@gmail.com>
2023-07-10 19:14:05 +0200

    NuovoPoll + Schermata fine

---
+++ app/src/main/java/com/example/greenapplication/screens/EndScreen.kt
@@ -0,0 +1,85 @@
+package com.example.greenapplication.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.greenapplication.R
+import com.example.greenapplication.pag1.*
+import com.example.greenapplication.ui.theme.GreenApplicationTheme
+import com.google.relay.compose.BoxScopeInstanceImpl.align
+import it.polito.did.gameskeleton.screens.GenericScreen
+import it.polito.did.gameskeleton.ui.theme.GameSkeletonTheme
+
+private val teamNames = listOf("Team 1", "Team 2", "Team 3", "Team 4")
+
+@Composable
+fun EndScreen(teamNames: String,
+              modifier: Modifier = Modifier)
+{
+    Box(
+        modifier = Modifier
+            .background(color = MaterialTheme.colors.background)
+            .fillMaxSize())
+
+        Column(
+            modifier.fillMaxWidth()) {
+            Spacer(Modifier.weight(1f))
+            Image (
+                painter = painterResource(R.drawable.logo),
+                contentDescription = "logo",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.align(Alignment.CenterHorizontally).size(300.dp).fillMaxWidth(1.0f).fillMaxHeight(1.0f)
+            )
+            Spacer(Modifier.height(32.dp))
+            androidx.compose.material.Text(
+                text = "La squadra vincitrice è:",
+                style = MaterialTheme.typography.subtitle2,
+                color = MaterialTheme.colors.surface,
+                textAlign = TextAlign.Left,
+                modifier = Modifier.padding(horizontal = 50.dp)
+            )
+            Spacer(Modifier.height(32.dp))
+            androidx.compose.material.Text(
+                text = teamNames,
+                style = MaterialTheme.typography.subtitle1,
+                color = MaterialTheme.colors.surface,
+                textAlign = TextAlign.Left,
+                modifier = Modifier.padding(horizontal = 50.dp)
+            )
+            Spacer(Modifier.height(22.dp))
+            androidx.compose.material.Text(
+                text = "Ottimo lavoro!",
+                style = MaterialTheme.typography.subtitle2,
+                color = MaterialTheme.colors.surface,
+                textAlign = TextAlign.Justify,
+                modifier = Modifier.padding(horizontal = 50.dp)
+            )
+
+            Spacer(Modifier.weight(1f))
+        }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewEndScreen() {
+    GameSkeletonTheme() {
+
+        EndScreen(teamNames = teamNames.elementAt(0))
+    }
+
+}
--- app/src/main/java/com/example/greenapplication/screens/PollScreen.kt
+++ app/src/main/java/com/example/greenapplication/screens/PollScreen.kt
@@ -1,6 +1,5 @@
 package com.example.greenapplication.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -9,74 +8,95 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.greenapplication.Infrastruttura
-import com.example.greenapplication.pag1.Text
 import it.polito.did.gameskeleton.ui.theme.GameSkeletonTheme
 
+
+val azioni = listOf("Aggiungere", "Smantellare")
+
 @Composable
-fun PollScreen(azione: String, infrastruttura: String){
+fun PollScreen(azioni: String, Name: String){
+
+
+    val proposte = listOf("Proposta1", "Proposta2", "Proposta3", "Proposta4", "Proposta5" )
+    val selectedProposte = remember { mutableStateOf("") }
+
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 450.dp, start = 15.dp, end = 15.dp)
+            .padding(top = 350.dp, start = 15.dp, end = 15.dp)
             .clickable { },
         elevation = 10.dp
-    ){
-        Column(
-            modifier = Modifier.padding(15.dp), horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            androidx.compose.material.Text(
-                text = "Sondaggio",
-                style = MaterialTheme.typography.subtitle1,
-                color = MaterialTheme.colors.background,
-                modifier = Modifier.padding(bottom = 10.dp)
-            )
+    )
 
-            androidx.compose.material.Text(
-                text = "Proposta ${azione} infrastruttura:",
-                style = MaterialTheme.typography.subtitle2,
-                color = MaterialTheme.colors.onBackground,
-                textAlign = TextAlign.Center
-            )
-            androidx.compose.material.Text(
-                text = "${infrastruttura}",
-                style = MaterialTheme.typography.body2,
-                color = MaterialTheme.colors.onBackground,
-                textAlign = TextAlign.Center
-            )
-            androidx.compose.material.Text(
-                text = "Sei favorevole a questa scelta? ",
-                style = MaterialTheme.typography.subtitle2,
-                color = MaterialTheme.colors.onBackground,
-                textAlign = TextAlign.Center
-            )
-
-            Row() {
-                Button(onClick = { /*TODO*/ },
-                    shape = RoundedCornerShape(20.dp),
-                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red),
-                modifier = Modifier.padding(25.dp)) {
-                    androidx.compose.material.Text(text = "", color = Color.White)
-                    Icon(imageVector = Icons.Default.Close, "",
-                        tint = Color.White)
-                }
-                Button(onClick = { /*TODO*/ },
-                    shape = RoundedCornerShape(20.dp),
-                    colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.background),
-                    modifier = Modifier.padding(25.dp)) {
-                    androidx.compose.material.Text(text = "", color = Color.White)
-                    Icon(imageVector = Icons.Default.Check, "",
-                        tint = Color.White)
+    {
+        Column (Modifier.padding(top = 10.dp, start = 15.dp, end = 15.dp)) {
+            Text(text = "Scegli l'opzione che preferisci: \n ${selectedProposte.value}", style = MaterialTheme.typography.body2,
+                color = MaterialTheme.colors.background,)
+            proposte.forEach { option ->
+                Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = selectedProposte.value == option,
+                        onClick = { selectedProposte.value = option },
+                        modifier = Modifier.padding(8.dp),
+                    )
+                    Text(text = option, style = MaterialTheme.typography.subtitle2,
+                        color = MaterialTheme.colors.onBackground, )
                 }
             }
         }
+
     }
+
+    /*Column(
+        modifier = Modifier.padding(15.dp), horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        androidx.compose.material.Text(
+            text = "Sondaggio",
+            style = MaterialTheme.typography.subtitle1,
+            color = MaterialTheme.colors.background,
+            modifier = Modifier.padding(bottom = 10.dp)
+        )
+
+        androidx.compose.material.Text(
+            text = "${azioni}:",
+            style = MaterialTheme.typography.subtitle2,
+            color = MaterialTheme.colors.onBackground,
+            textAlign = TextAlign.Center
+        )
+        androidx.compose.material.Text(
+            text = "${Name}",
+            style = MaterialTheme.typography.body2,
+            color = MaterialTheme.colors.onBackground,
+            textAlign = TextAlign.Center
+        )
+
+        Row() {
+            Button(onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(20.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red),
+            modifier = Modifier.padding(25.dp)) {
+                androidx.compose.material.Text(text = "", color = Color.White)
+                Icon(imageVector = Icons.Default.Close, "",
+                    tint = Color.White)
+            }
+            Button(onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(20.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.background),
+                modifier = Modifier.padding(25.dp)) {
+                androidx.compose.material.Text(text = "", color = Color.White)
+                Icon(imageVector = Icons.Default.Check, "",
+                    tint = Color.White)
+            }
+        } */
 }
 
 
@@ -86,6 +106,6 @@ fun PreviewPollScreen(){
     GameSkeletonTheme {
         MapScreen(SelectedIcon = 2)
         GridScreen(team = "Quartiere Rosso")
-        PollScreen("smantellamento", "Centrale Geotermica")
+        PollScreen(azioni.elementAt(0), strutture.elementAt(1).name.toString())
     }
 }
